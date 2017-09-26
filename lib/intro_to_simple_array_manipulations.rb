fa7381c2e6088677d4a44bcbd0f3cc260383824a
Winter LaMon <winter.lamon@gmail.com>
2017-09-26 21:11:43 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,3 +22,26 @@ def shift_with_args(array)
   array.shift(2)
 end
 
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(element, 4)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete[integer]
+end
